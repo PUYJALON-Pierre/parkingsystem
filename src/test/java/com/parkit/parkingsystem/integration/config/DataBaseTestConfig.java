fc0f60bbda9f6442330a737d6db4d92dca80c2c3
PUYJALON Pierre <puyjalon.pierre@gmail.com>
2023-01-07 11:03:23 +0100

    Todo#2 (Fix integration tests)
    Config access to database:
    - add Database info into log4j.properties
    - modification of get connection()method in order to retrieve urlDb,
    userDb and passwordDb from the log4j2.properties file
    - Rename log4j.properties into log4j2.properties
    - Fix issues in Ticket DAO
    
    Modification to:
    -DataBaseTestConfig.java
    -ParkingDataBaseIT.java (complete testParkingACar(),
    testParkingLotExit())

--- src/test/java/com/parkit/parkingsystem/integration/config/DataBaseTestConfig.java
+++ src/test/java/com/parkit/parkingsystem/integration/config/DataBaseTestConfig.java
@@ -4,49 +4,72 @@ import com.parkit.parkingsystem.config.DataBaseConfig;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.*;
+import java.util.Properties;
 
 public class DataBaseTestConfig extends DataBaseConfig {
 
-    private static final Logger logger = LogManager.getLogger("DataBaseTestConfig");
+	private static final Logger logger = LogManager.getLogger("DataBaseTestConfig");
 
-    public Connection getConnection() throws ClassNotFoundException, SQLException {
-        logger.info("Create DB connection");
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        return DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/test","root","rootroot");
-    }
+	public Connection getConnection() throws ClassNotFoundException, SQLException {
+		Connection connection = null;
+		logger.info("Create DB connection");
+		try {
+			FileInputStream fileInputStream = new FileInputStream("src/main/resources/log4j2.properties");
+			Properties properties = new Properties();
+			properties.load(fileInputStream);
 
-    public void closeConnection(Connection con){
-        if(con!=null){
-            try {
-                con.close();
-                logger.info("Closing DB connection");
-            } catch (SQLException e) {
-                logger.error("Error while closing connection",e);
-            }
-        }
-    }
+			String urlDbTest = properties.getProperty("urldbTest");
+			String user = properties.getProperty("userdb");
+			String password = properties.getProperty("userpassword");
 
-    public void closePreparedStatement(PreparedStatement ps) {
-        if(ps!=null){
-            try {
-                ps.close();
-                logger.info("Closing Prepared Statement");
-            } catch (SQLException e) {
-                logger.error("Error while closing prepared statement",e);
-            }
-        }
-    }
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			connection = DriverManager.getConnection(urlDbTest, user, password);
 
-    public void closeResultSet(ResultSet rs) {
-        if(rs!=null){
-            try {
-                rs.close();
-                logger.info("Closing Result Set");
-            } catch (SQLException e) {
-                logger.error("Error while closing result set",e);
-            }
-        }
-    }
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return connection;
+	}
+
+	public void closeConnection(Connection con) {
+		if (con != null) {
+			try {
+				con.close();
+				logger.info("Closing DB connection");
+			} catch (SQLException e) {
+				logger.error("Error while closing connection", e);
+			}
+		}
+	}
+
+	public void closePreparedStatement(PreparedStatement ps) {
+		if (ps != null) {
+			try {
+				ps.close();
+				logger.info("Closing Prepared Statement");
+			} catch (SQLException e) {
+				logger.error("Error while closing prepared statement", e);
+			}
+		}
+	}
+
+	public void closeResultSet(ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+				logger.info("Closing Result Set");
+			} catch (SQLException e) {
+				logger.error("Error while closing result set", e);
+			}
+		}
+	}
 }
